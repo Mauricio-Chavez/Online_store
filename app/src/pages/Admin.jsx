5d86de6c8cdd26c131c4275fc970e53ce412d1a0
Mauricio-Chavez <mau_chavez003@hotmail.com>
2024-03-30 22:45:44 -0700

    Class 2- Assignment 2

--- app/src/pages/Admin.jsx
+++ app/src/pages/Admin.jsx
@@ -1,15 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Admin.css'
 function Admin() {
+    const [coupon, setCoupon] = useState({
+        code:'',
+        discount:'',
+    });
+    const [product, setProduct] = useState({
+        title:'',
+        price:'',
+        category:'',
+        image:'',
+    });
+    const [allProducts, setAllProducts] = useState([]);
+    function handleProductChange(e) {
+        const val = e.target.value
+        const name = e.target.name
+        
+        var copy = {...product}
+        copy[name] = val;
+        setProduct(copy);
+    }
+    function saveProduct(e) {
+        e.preventDefault();
+        console.log(product);
+        setAllProducts([...allProducts, product]);
+    }
+    const [allCoupons, setAllCoupons] = useState([]);
+
+    function handleCouponChange(e) {
+        const val = e.target.value
+        const name = e.target.name
+        
+        var copy = {...coupon}
+        copy[name] = val;
+        setCoupon(copy);
+    }
+    function saveCoupon(e) {
+        e.preventDefault();
+        setAllCoupons([...allCoupons, coupon]);
+    }
     return (
         <div className='admin page'>
             <h1>Store Administration</h1>
-            <div className='parent'>
+            <div className='parent container'>
                 <div className='prods'>
-                    <h3>Products</h3>
+                    
+                    <form className='form'>
+                        <h3>Products</h3>
+                        <div className='mb-3'>
+                            <label className='form-label'>Product Name</label>
+                            <input type="text" className='form-control' name="title" onChange={handleProductChange}/>
+                        </div>
+                        <div className='mb-3'>
+                            <label className='form-label'>Product Price</label>
+                            <input type="number" className='form-control' name='price'onChange={handleProductChange}/>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Category</label>
+                            <input type="text" className="form-control" name='category' onChange={handleProductChange}/>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Image</label>
+                            <input type="text" className="form-control" name='image' onChange={handleProductChange}/>
+                        </div>
+                        
+                        <button type='submit' className='btn btn-outline-success' onClick={saveProduct}>Add Product</button>
+                    </form>
+                    <ul>
+                    <h3>List the Products</h3>
+                        {allProducts.map( p => <li ><b>Name: </b>{p.title} - <b>Price:</b> ${p.price} - <b>Category:</b> {p.category} - <b>Image:</b> {p.image}</li>)}
+                    </ul>
                 </div>
                 <div className='cupons'>
-                    <h3>Cupons</h3>
+                    
+                    <form action="" className='form'>
+                        <h3>Coupons</h3>
+                        <div className='mb-3'>
+                            <label className='form-label'>Coupon Code</label>
+                            <input type="text" className='form-control' onChange={handleCouponChange} name='code'/>
+                        </div>
+                        <div className='mb-3'>
+                            <label className='form-label'>Discount</label>
+                            <input type="number" className='form-control' onChange={handleCouponChange} name='discount'/>
+                        </div>
+                        <button type='submit' className='btn btn-outline-success' onClick={saveCoupon}>Save Coupon</button>
+                    </form>
+                    <ul>
+                    <h3>List the Coupons</h3>
+                        {allCoupons.map( c => <li ><b>Code: </b>{c.code} - <b>Discount:</b> {c.discount}% </li>)}
+                    </ul>
                 </div>
             </div>
         </div>
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
         <div className='home page'>
-            <h3>This is the Home Page</h3>
+            <h3>Welcome to Stark Store</h3>
+            <Link to='/catalog' className='btn btn-outline-success'>View Catalog{'>'}</Link>
         </div>
     )
 }
